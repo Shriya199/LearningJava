16e9218ae8e9206519877f6e42a903f45ac20d5f
Shriya199 <pshriya9893@gmail.com>
2025-01-07 15:07:20 +0530

    HashMap implementation

---
+++ hashmapImplementation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class hashmapImplementation {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hm = new HashMap<>();
+
+        // Adding key-value pairs to the HashMap
+        hm.put(101, "Shriya");
+        hm.put(102, "Shriya");
+        hm.put(103, "Shriya");
+
+        // Print the HashMap
+        System.out.println(hm);
+    }
+}
